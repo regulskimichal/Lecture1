406ac91b1be6daf3d8030365660110c3f0a758c3
Michał Regulski <regulskimichal@outlook.com>
2022-07-13 21:38:52 +0200

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,18 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-plugins {
-    kotlin("jvm") version "1.7.0"
-}
-
 group = "pl.michalregulski"
 version = "1.0-SNAPSHOT"
+
+plugins {
+    java
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    testImplementation("junit:junit:4.13.2")
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
 }
